2511ebe9ce66add70d1b67932a68a87228b884ed
debug-log <achileus2003@naver.com>
2018-05-11 18:51:40 +0900

    d

--- cat_rest/urls.py
+++ cat_rest/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 from myapp.views import *
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^user/$', UserList.as_view()),
     url(r'^user/(?P<pk>[a-z0-9]+)/$', UserDetail.as_view()),
-    url(r'^dietinfo/$', DietInfoList.as_view()),
-    url(r'^dietinfo/(?P<pk>[a-z0-9]+)/$', DietInfoDetail.as_view()),
+    url(r'^dietinfo/$', csrf_exempt(DietInfoList.as_view())),
+    url(r'^dietinfo/(?P<pk>[a-z0-9]+)/$', csrf_exempt(DietInfoDetail.as_view())),
     url(r'^keyboard/', on_init),
     url(r'^message', on_message),
     url(r'^friend', on_added),
--- myapp/views/DietInfoView.py
+++ myapp/views/DietInfoView.py
@@ -1,14 +1,11 @@
 from myapp.models import *
 from myapp.serializers import *
 from rest_framework import generics
-from django.views.decorators.csrf import csrf_exempt
 
 class DietInfoList(generics.ListCreateAPIView):
-	@csrf_exempt
     queryset = DietInfo.objects.all()
     serializer_class = DietInfoSerializer
 
 class DietInfoDetail(generics.RetrieveDestroyAPIView):
-	@csrf_exempt
     queryset = DietInfo.objects.all()
     serializer_class = DietInfoSerializer
