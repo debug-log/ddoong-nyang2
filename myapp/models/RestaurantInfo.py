cf3addc1076789129921ecc66f41eb1e53bdf5ba
debug-log <achileus2003@naver.com>
2018-05-24 21:37:32 +0900

    d

--- myapp/models/RestaurantInfo.py
+++ myapp/models/RestaurantInfo.py
@@ -3,7 +3,7 @@ from django.db import models
 class RestaurantInfo(models.Model):
     name = models.CharField(max_length=20, primary_key=True)
     text_date_day = models.TextField()
-    text_date_holiday = models.TextField()
+    text_date_holiday = models.TextField(null = True)
 
     def __str__(self):
         return self.name
