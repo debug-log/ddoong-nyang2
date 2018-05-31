76ff3c5f9ee22c8d67f87481b77321ceb4f7bdf1
debug-log <achileus2003@naver.com>
2018-05-31 16:09:15 +0900

    8

--- myapp/models/User.py
+++ myapp/models/User.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=20, primary_key=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    last_request = models.IntegerField(null=True)
+    last_request = models.IntegerField(null = False, default = 0)
     content = models.TextField(null=True)
 
     def __str__(self):
